3cc26efbab7824cde388afdf3782bdc7fbac6ba9
Dusan <root@ndusan.(none)>
2011-01-02 12:57:45 +0100

    Spacethunders project

--- index.php
+++ index.php
@@ -2,11 +2,69 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta name="keywords" content="Spacethunders.com hub" />
+        <meta name="description" content="Spacethunders.com is hub for all dev projects by Dušan Novaković" />
+        <title>Spacethunders HUB by Dušan Novaković</title>
+        <!--jQuery -->
+        <script src="public/js/jquery-1.4.4.min.js" type="text/javascript"></script>
+        <!--Default js -->
+        <script src="public/js/default.js" type="text/javascript"></script>
+        <!--Default css -->
+        <link rel="stylesheet" media="screen" href="public/css/default.css"/>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div id="header">
+            <h3>Spacethunders.com HUB by <a href="http://novakovicdusan.com" target="_blank">Dušan Novaković</a></h3>
+        </div>
+        <div id="content">
+            <div class="inner-boundary">
+                <ul class="item-list">
+                    <li>
+                        <h3 class="long-title">
+                            <a href="http://belvi.spacethunders.com" target="_blank">Belvi</a>
+                        </h3>
+                        <p class="metadata">Web site for travel agency with CMS backend engine</p>
+                        <div class="clear"></div>
+                    </li>
+                    <li>
+                        <h3 class="long-title">
+                            <a href="http://glosarij.spacethunders.com" target="_blank">Glosarij</a>
+                        </h3>
+                        <p class="metadata">Web site for pharmacy company with CMS backend engine</p>
+                        <div class="clear"></div>
+                    </li>
+                    <li>
+                        <h3 class="long-title">
+                            <a href="http://travelboutique.spacethunders.com" target="_blank">Travelboutique</a>
+                        </h3>
+                        <p class="metadata">Web site for travel agency with CMS backend engine</p>
+                        <div class="clear"></div>
+                    </li>
+                    <li>
+                        <h3 class="long-title">
+                            <a href="http://likvidamedel.spacethunders.com" target="_blank">Likvidamedel</a>
+                        </h3>
+                        <p class="metadata">Web site for Swedish company with CMS backend engine for full control of content on all pages, etc.</p>
+                        <div class="clear"></div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!--footer-->
+        <div id="footer">
+            <div class="container">
+                This site presents HUB of all projects in development phase. The main idea is to present application to client while it's in development.
+            </div>
+            <div class="clear"></div>
+        </div>
+        <!--copyrights-->
+        <div id="copyright">
+            <div class="container">
+                <div class="copyright">
+                    <p>The applications were built using PHP programming language and JavaScript (jQuery library), in the combination with MySQL data base. This makes the application reliable and highly secured from a possible unauthorized system breach or unwanted data loss.</p>
+                    <p class="trademarks">All copyrights reserved by <a href="http://novakovicdusan.com">Dušan Novaković</a></p>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
